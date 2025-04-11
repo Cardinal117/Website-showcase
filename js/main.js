a68268d1b92749396b0ff3df0e37c0106c516484
J.B <jaydenlberg2005@gmail.com>
2025-04-11 09:25:46 +0200

    Force updating

--- js/main.js
+++ js/main.js
@@ -10,21 +10,21 @@ let developmentArr = [];
 let savedItems = [];
 
 let codeSlide1 = new slide(
-  `Images/C_cheatSheet_mousePad.png`,
+  `dammm/C_cheatSheet_mousePad.png`,
   `If your looking for a new mouse pad I recommend this one.`
 );
 let codeSlide2 = new slide(
-  `Images/Benefits-of-Version-Control.png`,
+  `dammm/Benefits-of-Version-Control.png`,
   `I highly recommend checking out <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiVorjKlfOLAxV4SfEDHciELrcQFnoECAsQAQ&url=https%3A%2F%2Fgithub.com%2F&usg=AOvVaw38IHvcyBra8HGhmSxvlCGw&opi=89978449">version control</a>, using it will allow you to have backups for everything.`
 );
 let developmentSlide1 = new slide(
-  "Images/Color-Switch.png",
+  "dammm/Color-Switch.png",
   "Starting small and working your way up is the best way to learn, " +
     "start by making well known mobile games and improve upon your skills " +
     "using them as a basis."
 );
 let developmentSlide2 = new slide(
-  "Images/flappy_bird.gif",
+  "dammm/flappy_bird.gif",
   "a Traditional game to start with is flappy bird, you'll learn" +
     " one of the first great things about programming, how to fool the " +
     "audience. Did you know that it is not actually the bird that is moving " +
